b1259602dcd70b6de385d54c22998a1f5dbbc9c7
Jesus Legaspy <jesus.legaspy@gmail.com>
2023-09-19 17:27:17 -0500

    Recurive version of mergeTwoSortedLists

--- mergeTwoSortedLists.py
+++ mergeTwoSortedLists.py
@@ -1,34 +1,45 @@
 class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+  def __init__(self, val=0, next=None):
+      self.val = val
+      self.next = next
+
+# def mergeTwoLists(list1, list2):
+#   first = ListNode
+#   result = first
+
+#   while(True):
+#     if(list1 == None):
+#       result.next = list2
+#       break
+#     if (list2 == None):
+#       result.next = list1
+#       break
+
+#     if list1.val <= list2.val:
+#       result.next = list1
+#       result = result.next
+#       list1 = list1.next
+#       continue
+#     if list1.val > list2.val:
+#       result.next = list2
+#       result = result.next
+#       list2 = list2.next
+#       continue
+
+#   return first.next
 
 def mergeTwoLists(list1, list2):
-  first = ListNode
-  result = first
+  if list1 == None:
+    return list2
+  
+  if list2 == None:
+    return list1
 
-  while(True):
-    if(list1 == None):
-      result.next = list2
-      break
-    if (list2 == None):
-      result.next = list1
-      break
-
-    if list1.val <= list2.val:
-      result.next = list1
-      result = result.next
-      list1 = list1.next
-      continue
-    if list1.val > list2.val:
-      result.next = list2
-      result = result.next
-      list2 = list2.next
-      continue
-
-  return first.next
-
-
+  if list1.val < list2.val:
+    list1.next = mergeTwoLists(list1.next, list2)
+    return list1
+  list2.next = mergeTwoLists(list1, list2.next)
+  return list2
 
 list1 = ListNode(1, ListNode(2, ListNode(4)))
 list2 = ListNode(1, ListNode(3, ListNode(4)))
