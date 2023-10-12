7e17b17d649dbb5b0ff6a7c03f00101931113d2c
Jesus Legaspy <jesus.legaspy@gmail.com>
2023-10-12 16:51:50 -0500

    problem 28

---
+++ findTheIndexOfTheFirstOccurrenceInAString.py
@@ -0,0 +1,8 @@
+class Solution:
+  # Returns an int
+  def strStr(haystack, needle):
+    return haystack.find(needle)
+
+
+print(Solution.strStr("sadbutsad", "sad"))
+print(Solution.strStr("leetcode", "leeto"))
