ff4dc97f8e52dfaf52c8ae157f3c77d65e11ba43
Jesus Legaspy <jesus.legaspy@fastmail.com>
2023-10-19 17:13:43 -0500

    First couple of attempts

--- 69_sqrt_x.py
+++ 69_sqrt_x.py
@@ -2,8 +2,44 @@ import math
 
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return math.floor(math.sqrt(x))
+        # Iterate through squares
+        # Once the square is greater than x, the solution is the square - 1.
+
+        # square = 1
+        # while True:
+        #     if ( x < square * square):
+        #         return square - 1
+        #     square = square + 1
+
+        ###
+
+        if (x == 0):
+            return 0
+
+        def find(lower: int, upper: int):
+            if(upper-lower <= 1):
+                return lower
+
+            mid = (upper - lower) // 2 + lower
+            square = mid * mid
+            if (square == x):
+                return mid
+            if (square < x):
+                return find(mid, upper)
+            else:
+                return find(lower, mid)
+        
+        return(find(1, x//2 + 1))
 
 
-solution = Solution()
-print(solution.mySqrt(4))
+print(Solution().mySqrt(1))
+print(Solution().mySqrt(4))
+print(Solution().mySqrt(9))
+print(Solution().mySqrt(16))
+print(Solution().mySqrt(64))
+print(Solution().mySqrt(63))
+print(Solution().mySqrt(1024))
+print(Solution().mySqrt(1023))
+print(Solution().mySqrt(10000))
+print(Solution().mySqrt(9999))
+
