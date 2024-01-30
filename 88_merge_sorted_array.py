80c5d4e799da8661c5299cca8968e5cd00712bf4
Jesus Legaspy <jesus.legaspy@fastmail.com>
2024-01-30 11:44:04 -0600

    My merge sorted array solution

--- 88_merge_sorted_array.py
+++ 88_merge_sorted_array.py
@@ -5,35 +5,34 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        newArr = []
-        mdx = 0
-        ndx = 0
-        while(True):
-          if(mdx >= m and ndx >= n):
-            break
-          if(mdx>= m):
-            newArr.append(nums2[ndx])
-            ndx = ndx + 1
-            continue
-          if(ndx>=n):
-            newArr.append(nums1[mdx])
-            mdx = mdx + 1
-            continue
-          if(mdx>= m  or nums2[ndx] < nums1[mdx]):
-            newArr.append(nums2[ndx])
-            ndx = ndx + 1
-            continue
-          if(ndx>=n or nums1[mdx] <= nums2[ndx]):
-            newArr.append(nums1[mdx])
-            mdx = mdx + 1
-            continue
-        
-        for idx in range(len(nums1)):
-          nums1[idx] = newArr[idx]
+        for idx in range(len(nums1)-1, -1, -1):
+            if not (m != 0 and n==0) and ((m == 0 and n != 0) or nums1[m-1] < nums2[n-1]):
+                nums1[idx] = nums2[n-1]
+                n = n -1
+            else:
+                nums1[idx] = nums1[m-1]
+                m = m - 1
+
+
+       
+
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+Solution().merge(nums1, m, nums2, n)
+print(nums1)
 
 nums1 = [1]
 m = 1
 nums2 = []
 n = 0
+Solution().merge(nums1, m, nums2, n)
+print(nums1)
+
+nums1 = [0]
+m = 0
+nums2 = [1]
+n = 1
 Solution().merge(nums1, m, nums2, n)
 print(nums1)
